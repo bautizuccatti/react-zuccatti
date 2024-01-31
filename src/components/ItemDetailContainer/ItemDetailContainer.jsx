1d8ee62bd3b2f9be3758f8b5c95091d24c2672d0
bautista zuccatti <zuccattibautista@gmail.com>
2024-01-31 15:05:53 -0300

    proyecto honky tonk zuccatti

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,31 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import { pedirDatos } from '../../utils/utils';
-import ItemDetail from '../ItemDetail/ItemDetail';
-import { useParams } from 'react-router-dom';
-import Loader from '../Loader/Loader';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import ItemDetail from "../ItemDetail/ItemDetail";
+import Loader from "../Loader/Loader";
+import { db } from "../../firebase/config";
+import { doc, getDoc } from "firebase/firestore";
 
 const ItemDetailContainer = () => {
-  const [loading, setLoading] = useState(true);
-  const [item, setItem] = useState(null);
-  const { itemId } = useParams(); 
+    const [loading, setLoading] = useState(true);
+    const [item, setItem] = useState(null);
 
-  useEffect(() => {
-    pedirDatos(true)
-      .then((data) => {
-        setItem(data.find((prod) => prod.id === Number(itemId)));
-      })
-      .finally(() => setLoading(false));
-  }, [itemId]); 
+    const { itemId } = useParams()
 
-  return (
+    useEffect(() => {
+        setLoading(true);
+
+        const docRef= doc(db, 'productos', itemId)
+        getDoc(docRef)
+            .then((docSnapshot) => {
+                const doc = {
+                    ...docSnapshot.data(),
+                    id: docSnapshot.id
+                }
+                setItem(doc)
+            })
+            .finally (() => setLoading(false))
+    }, [itemId]); 
+
+    return (
     <>
-      {loading ? (
+        {loading ? (
         <Loader />
       ) : (
-        item && <ItemDetail item={item} />
-      )}
+            <ItemDetail item={item}/>
+        )}
     </>
-  );
+    );
 };
-
-export default ItemDetailContainer;
+    export default ItemDetailContainer;
